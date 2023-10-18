beb64d302155e52bab4d2513c7755a87a762200d
castrogarciajs <johancs.mm@gmail.com>
2023-10-18 15:21:23 -0500

    feat(feat): add merge sorted

---
+++ tsc/merge_sorted.test.ts
@@ -0,0 +1,12 @@
+import { assertEquals } from 'https://deno.land/std@0.204.0/assert/assert_equals.ts'
+import { mergeSortedArray } from './merge_sorted.ts'
+
+Deno.test('mergeSortedArray', () => {
+	const testOne = mergeSortedArray([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+	const expected = [1, 2, 2, 3, 5, 6]
+	const testTwo = mergeSortedArray([1], 1, [], 0)
+	const expectedTwo = [1]
+
+	assertEquals(testOne, expected)
+	assertEquals(testTwo, expectedTwo)
+})
